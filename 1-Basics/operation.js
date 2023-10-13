a453ff407d28e151dd5e49d03adc364bb8320d66
Sandeepsh89 <147059035+Sandeepsh89@users.noreply.github.com>
2023-10-13 12:52:57 +0000

    opeartion done

---
+++ 1-Basics/operation.js
@@ -0,0 +1,40 @@
+let value = 3
+let nagValue = -value
+// console.log(nagValue);
+// console.log(2+3)
+// console.log(2**5); // 2 to power 5
+// console.log(10-5);
+let str1 = "Hello"
+let str2 = " Sandeep"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + 3 + "2");
+// console.log("1" + 3 + 2);
+
+// console.log(+true); // increament vaule = 1
+// console.log(true+); // unexpected Token = Not Allowed
+
+
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+// ++ --> Precedence opeartor
+// ++ variable=> prefix
+// variable++ => postfix --> read MDN (IMP)
+
+
+// ******************************
+// Postfix ++ increment operator
+
+let x = 3;
+const y = x++;
+// console.log(`x:${x}, y:${y}`);
+
+// prefix ++ increment operator
+
+let a = 2;
+const b = ++x;
+console.log(a , b); // wrong
